4a484cdb9aed27a743e629f0788801e74229c7a2
DanielFrancoFlores <danielrigoberto020@gmail.com>
2025-03-06 08:38:06 -0700

    Prueba de commit aqui en la escuela

---
+++ EJERCICIOS/ejercicio1.c
@@ -0,0 +1,25 @@
+//Danie Rigoberto Franco Flores
+//04/03/2025
+#include <stdio.h>
+
+int main(){
+	float lado, area_base, area_lateral, area_total, volumen;
+
+	printf("Ingrese la longitud del lado del cubo: \n");
+	scanf("%f", &lado);
+
+	area_base = lado * lado;
+	area_lateral = 4 * area_base;
+	area_total = 6 * area_base;
+	volumen = lado * lado * lado;
+
+	printf("\nResultados:\n");
+	printf("Área de la base: %f\n", area_base);
+	printf("Área lateral: %f\n", area_lateral);
+	printf("Área total: %f\n", area_total);
+	printf("Volumen: %f\n", volumen);
+
+	return 0;
+}
+
+
