307262364af753fc7669a981124b58dba0766818
JohnFelix4 <je.delangel23@info.uas.edu.mx>
2025-03-10 08:54:04 -0700

    Ejercicio 1

---
+++ ejercicios/Ejercicio1-1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	//VARIABLES
+	int a,b;
+	int division;
+	int restos;
+
+	//ENTRADA DE DATOS
+	printf("Ingresa el valor de A: \n");
+	scanf("%d",&a);
+	printf("Ingresa el valor de A: \n");
+	scanf("%d",&b);
+	//OPERACION
+	division= b/a;
+	restos=
+
+	//IMPRIMIR LOS RESULTADOS
+	printf("%d",division);
+
+}
