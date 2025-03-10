faa044df6dc9a46b5910d3219dd56eded6865b23
Santiago Haros <js.haros23@info.uas.edu.mx>
2025-03-10 08:53:56 -0700

    empezando ejercicios pag 70-71

---
+++ ejercicios02/ejercicio1.c
@@ -0,0 +1,25 @@
+/*Dados dos valores enteros (a, b) determinar el valor
+de la división entera y el resto de la división entera
+de los mismos (a, b), sin usar los operadores de
+división entera ó el de residuo.*/
+#include <stdio.h>
+int main()
+{
+    int a, b, count;
+    
+    printf("Proporcione el valor de a: ");
+    scanf("%d", &a);
+    printf("Proporcione el valor de a: ");
+    scanf("%d", &b);
+
+    //calcular cuantas veces cabe b en a falta
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+    //salida
+    printf("El valor de la division entera de a es de: %d y el valor del resto es de: %d\n");
+    
+    return 0;
+}
