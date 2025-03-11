faccc11c37b97584ed709cd75d9c4f28aa4b11a7
ALumnos <alumnos@B29>
2025-03-11 08:50:59 -0700

    ejercicios

--- ejercicios/hexaedro.c
+++ ejercicios/hexaedro.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
-void main(){
-int l,a,ar,at,v;
-	printf("ingrese la dimension de la arista del hexaedro");
-	scanf("%d",&l);
-	a= l * l;
-	ar=
+int main(){
+	float lado, area_base, area_lateral, area_total, volumen;
+
+	 printf("Ingrese la longitud del lado del hexaedro: \n");
+	 scanf("%f", &lado);
+
+	area_base = lado * lado;
+	area_lateral = 4 * area_base;
+	area_total = 6 * area_base;
+	volumen = lado * lado * lado;
+
+	 printf("\nResultados:\n");
+	 printf("Área de la base: %f\n", area_base);
+	 printf("Área lateral: %f\n", area_lateral);
+	 printf("Área total: %f\n", area_total);
+	 printf("Volumen: %f\n", volumen);
+
+	return 0;
+}
