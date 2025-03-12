50a02db863c6c8ab7e13c9d0684052c1d97fb781
Gustavo Murillo Vega <g.murillo24@info.uas.edu.mx>
2025-03-12 08:15:18 -0700

    ejercicio 5

---
+++ ejercicios_p70_71/5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+double pow(double x, int n) {
+	if (n < 0) {
+		x = 1/x;
+		n = -n;
+	}
+	double total = 1;
+	for (int i=0; i<n; i++) {
+		total *= x;
+	}
+	return total;
+}
+
+int main() {
+	printf("Ingrese un número x cualquiera y otro n entero:\n");
+	double x;
+	int n;
+	scanf("%lf %d", &x, &n);
+	double suma = 1;
+	for (int i=1; i<n; i++) {
+		suma += pow(x,i);
+		printf("\r%lf", suma);
+	}
+	printf("\n");
+	printf("1+x+x^2+…+x^n: %f\n", suma);
+}
