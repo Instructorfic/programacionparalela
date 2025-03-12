72e2a5d88bae9a8e1b868ca11bf8d8d8b328e1ea
YukiAlv <dy.alvarez23@info.uas.edu.mx>
2025-03-12 08:36:48 -0700

    actualizacion de ejercicio

---
+++ ejercicios/ejercicio01_pag70.c
@@ -0,0 +1,16 @@
+/*
+El programa dados dos valores enteros (a, b) determinar el valor de la división entera y el resto de la división entera de los mismos (a, b), sin usar los operadores de
+división entera ó el de residuo.
+Fecha: 11/03/2025
+*/
+#include <stdio.h>
+int main(){
+	int a, b;
+	int division;
+	int residuo;
+	printf("Ingresa el valor de a y el de b: ");
+	scanf("%d", &a);
+	scanf("%d", &b);
+
+	printf("%*d", a);
+}
