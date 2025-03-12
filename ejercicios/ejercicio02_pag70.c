95c3ff471e387281cd4c202b9dac9640db9533f7
YukiAlv <dy.alvarez23@info.uas.edu.mx>
2025-03-12 09:00:01 -0700

    actualizacion de ejercicios

---
+++ ejercicios/ejercicio02_pag70.c
@@ -0,0 +1,10 @@
+/*
+El programa realizara un algoritmo que lea 3 números diferentes y determine el número medio del conjunto de los 3 números.
+Fecha: 12/03/2025
+*/
+#include <stdio.h>
+
+int main(int ar){
+
+
+}
---
+++ ejercicios/ejercicio03_pag70.c
@@ -0,0 +1,10 @@
+/*
+Crear un programa que lea una hora (hora, minutos, segundos) y diga la hora que será un segundo después.
+Fecha: 12/03/2025
+*/
+
+#include <stdio.h>
+int main(){
+
+
+}
