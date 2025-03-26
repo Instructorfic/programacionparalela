6d1d199a3aede742d3aa785a9313b80859c6a4fa
Raacruz <rf.cruz23@info.uas.edu.mx>
2025-03-25 21:54:59 -0700

    Ejercicio apuntadores

---
+++ ejercicios/rotar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void rotar(int*a,int*b,int*c);
+int main(){
+	int a=5,b=2,c=3;
+	printf("Valores sin rotar\n");
+	printf("a=%d, b=%d, c=%d\n",a,b,c);
+	rotar(&a,&b,&c);
+	printf("Valores rotados\n");
+	printf("a=%d, b=%d, c=%d\n",a,b,c);
+	return 0;
+}
+
+void rotar(int*a,int*b,int*c){
+	int temp;
+	temp=*a;
+	*a=*b;
+	*b=*c;
+	*c=temp;
+
+}
+
+
