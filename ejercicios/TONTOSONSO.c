31298366b7d82a110a0d4ae8d3069418a54c5250
Urkiller <jg.urquidez23@info.uas.edu.mx>
2025-04-08 08:46:58 -0700

    Trabajo

---
+++ ejercicios/TONTOSONSO.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+for(int i=0;;) {
+printf("Hola soy el sigma alfalobo del brawl star y ando buscando una mikunena como tu grrrr, si o no mamu");
+}
+return 0;
+
+}
