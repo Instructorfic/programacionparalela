480e81953482d20d6184fb85a85891c7d142540b
carlosrga <cr.gonzales23@info.uas.edu.mx>
2025-05-13 09:00:59 -0700

    bien

--- ejercicios/vector.c
+++ ejercicios/vector.c
@@ -2,17 +2,17 @@
 #include<omp.h>
 
 int main(){
-int tamano=30;
+int tamano=10;
 int numeros[tamano];
 
 #pragma omp parallel
 {
- for(int i=0;i<tamano+1;i++){
-	numeros[i]=omp_get_thread_num();
-	printf("%d=%d\n",i,numeros[i]);
+#pragma omp for
+ for(int i=0;i<tamano;i++){
+	numeros[i]=i*i;
+	printf("Hilo %d escribio arreglo[%d]=%d\n",omp_get_thread_num(),i,numeros[i]);
  }
 }
-
 
 return 0;
 }
