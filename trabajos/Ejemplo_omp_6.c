3f5f2449c86c2535a1d079dd5bea34e047169b1e
Daniel Flores <145934980+DanielFrancoFlores@users.noreply.github.com>
2025-05-12 17:51:11 -0700

    Create Ejemplo_omp_6.c
    Ejercicio de clase 12/05/2025

---
+++ trabajos/Ejemplo_omp_6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    int totalElementos = 8;
+    int datosEntrada[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int datosSalida[totalElementos];
+
+
+    #pragma omp parallel for
+    for (int i = 0; i < totalElementos; i++) {
+        datosSalida[i] = datosEntrada[i] * datosEntrada[i];
+        printf("Hilo %d: %d^2 = %d\n", omp_get_thread_num(), datosEntrada[i], datosSalida[i]);
+    }
+
+    return 0;
+}
