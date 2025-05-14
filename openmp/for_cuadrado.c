3baca96c4a986845cb67526f542de6356ac32382
Gustavo Murillo Vega <g.murillo24@info.uas.edu.mx>
2025-05-14 08:43:40 -0700

    Funciona for_cuadrado.c

--- openmp/for_cuadrado.c
+++ openmp/for_cuadrado.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <omp.h>
 #define N_ELEMENTOS 20
-#define N_HILOS 16
 
 int main() {
     int a[N_ELEMENTOS];
-    int n_hilo[N_HILOS];
+    int n_hilo[N_ELEMENTOS];
 
-    omp_set_num_threads(N_HILOS);
+    // #ifndef OMP_NUM_THREADS
+    // omp_set_num_threads(16);
+    // #endif
+
+    printf("Usando %d hilos\n", omp_get_max_threads());
 
     #pragma omp parallel
     #pragma omp for
-    for (int i=0; i<N_ELEMENTOS || i<N_HILOS; i++) {
+    for (int i=0; i<N_ELEMENTOS; i++) {
         a[i] = i*i;
         n_hilo[i] = omp_get_thread_num();
-        printf("Se asignó %d a elemento de arreglo %d con hilo %d\n", a[i], i, n_hilo[i]);
+        // printf("Se asignó %d a elemento de arreglo %d con hilo %d\n", a[i], i, n_hilo[i]);
     }
 
-    for (int i=0; i<N_ELEMENTOS || i<N_HILOS; i++) {
-        printf("a[%d] = %d, escrito por hilo %d\n", i, a[i], n_hilo[i]);
+    for (int i=0; i<N_ELEMENTOS; i++) {
+        printf("a[%.2d] = %d, escrito por hilo %d\n", i, a[i], n_hilo[i]);
+    }
 }
