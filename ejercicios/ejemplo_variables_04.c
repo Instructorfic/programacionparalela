84d11de344b658cc0f31a3ffb3829b3cc9a12cd3
MarioCastillo <marioclopez13@gmail.com>
2025-05-20 08:57:30 -0700

    ejejrcicio sobre pragma critical,Atomic y reduction

---
+++ ejercicios/ejemplo_variables_04.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <omp.h>
+
+
+int main(){
+	int contador = 0;
+	int suma = 0;
+	#pragma omp parallel for reduction(+:suma)
+	for (int i = 0; i < 1000; i++) {
+	
+	contador++;
+	suma += i * 2;
+	}
+	printf("VAlor final de suma es: %d\n", suma);
+	return 0;}
