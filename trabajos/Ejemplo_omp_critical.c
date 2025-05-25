3282295d6e785f266e5ef0b66baa4d8b4890003a
Daniel Flores <145934980+DanielFrancoFlores@users.noreply.github.com>
2025-05-25 12:49:17 -0700

    Create Ejemplo_omp_critical.c
    Ejemplo de omp pasando variables como critico

---
+++ trabajos/Ejemplo_omp_critical.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    int contador = 0;
+
+    #pragma omp parallel
+    {
+        for (int i = 0; i < 100; i++) {
+            #pragma omp critical
+            {
+                contador++;
+            }
+        }
+    }
+
+    printf("Valor final del contador: %d\n", contador);
+    return 0;
+}
